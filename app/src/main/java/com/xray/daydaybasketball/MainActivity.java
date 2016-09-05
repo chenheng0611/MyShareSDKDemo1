d0e889abeab5139105816bc5d4c51a92ed6b31ba
hengheng <chenheng0611@163.com>
2016-09-05 16:58:30 +0800

    first commit111

--- app/src/main/java/com/xray/daydaybasketball/MainActivity.java
+++ app/src/main/java/com/xray/daydaybasketball/MainActivity.java
@@ -1,13 +1,98 @@
 package com.xray.daydaybasketball;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mShareText;
+    private ImageView mShareImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShareText = (EditText)findViewById(R.id.share_et);
+        mShareImage = (ImageView)findViewById(R.id.image_view);
+
+        ShareSDK.initSDK(this);
+    }
+
+    public void onShareClick(View view) {
+        showShare();
+    }
+
+    private void showShare() {
+
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，邮箱、印象笔记、信息、微信、人人网和QQ空间使用
+        oks.setTitle("hello");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+//        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+//        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath(path);//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+//        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
+    }
+
+    public void onAddImageClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+    String path;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            try {
+                ContentResolver resolver = getContentResolver();
+                InputStream inputStream = resolver.openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                mShareImage.setImageBitmap(bitmap);
+                Cursor cursor = resolver.query(uri, null, null, null, null);
+                if(cursor.moveToFirst()){
+                    path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                    Log.i("xxx", "onActivityResult: "+path);
+                }
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
